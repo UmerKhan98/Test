ce4692a875d0935d079ff6f2e62f32be576cd00f
UmerKhan98 <39845028+UmerKhan98@users.noreply.github.com>
2020-03-26 17:07:07 +0500

    move private information into dev.js

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const { User } = require("./models/user");
+
+const config = require("./config/key");
 
 mongoose
-  .connect("mongodb://localhost:27017/mern", {
+  .connect(config.mongoURI, {
     useNewUrlParser: true
   })
   .then(() => {
@@ -12,8 +17,21 @@ mongoose
   .catch(err => {
     console.error(err);
   });
+mongoose.set("useCreateIndex", true);
 
-app.get("/", (req, res) => {
-  res.send("Allah u Akbar");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+app.post("/api/users/register", (req, res) => {
+  const user = new User(req.body);
+
+  user.save((err, userData) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true
+    });
+  });
 });
+
 app.listen(7000);
